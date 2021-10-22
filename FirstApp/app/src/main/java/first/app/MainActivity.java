fc5271e5b502bb5df58dec6c61a9448d3ea43a98
sarbanandadev <sarbanandadev@gmail.com>
2021-10-22 12:12:07 +0600

    second commit

--- FirstApp/app/src/main/java/first/app/MainActivity.java
+++ FirstApp/app/src/main/java/first/app/MainActivity.java
@@ -7,15 +7,14 @@ import first.app.R;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        WebView webView = (WebView)findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/app/index.html");
-
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    
+    WebView webView = (WebView)findViewById(R.id.webView);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl("file:///android_asset/app/index.html");
+  }
 
 }
